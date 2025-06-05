84fe37ea70f7bab91ffc10c5741978df9feecd7e
NielsGrrr <Niels.Stiefvater@etu.univ-smb.fr>
2025-06-05 13:35:39 +0200

    classes dataaccess, employe, logerror + editorconfig

---
+++ Pilot/Classes/Employe.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pilot.Classes
+{
+    public class Employe
+    {
+        private int numEmploye;
+        private int numRole;
+        private string nom;
+        private string prenom;
+        private string password;
+        private string login;
+    }
+}
