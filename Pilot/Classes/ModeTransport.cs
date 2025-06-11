8275b2a5fe7d9c1f18510400c192aa38915a7be8
mathis-lanfroy <Mathis.Lanfroy@etu.univ-smb.fr>
2025-06-11 17:22:27 +0200

    Creation de commande mais bugué

--- Pilot/Classes/ModeTransport.cs
+++ Pilot/Classes/ModeTransport.cs
@@ -1,12 +1,14 @@
-﻿using System;
+﻿using Npgsql;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Pilot.Classes
 {
-    public class ModeTransport
+    public class ModeTransport: ICrud<ModeTransport>
     {
         private Int32 numTransport;
         private string libelleTransport;
@@ -45,6 +47,46 @@ namespace Pilot.Classes
             {
                 this.libelleTransport = value;
             }
+        }
+
+        public int Create()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ModeTransport> FindAll()
+        {
+            List<ModeTransport> lesTransports = new List<ModeTransport>();
+            using (NpgsqlCommand cmdSelect = new NpgsqlCommand("select * from modeTransport mt;"))
+            {
+                DataTable dt = DataAccess.Instance.ExecuteSelect(cmdSelect);
+                foreach (DataRow dr in dt.Rows)
+                {
+                    ModeTransport mt = new ModeTransport((Int32)dr["numtransport"], (String)dr["libelletransport"]);
+                    lesTransports.Add(mt);
+                }
+                return lesTransports;
+            }
+        }
+
+        public List<ModeTransport> FindBySelection(string criteres)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update()
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- Pilot/Windows/WindowCommande.xaml.cs
+++ Pilot/Windows/WindowCommande.xaml.cs
@@ -24,6 +24,7 @@ namespace Pilot.Windows
         public ObservableCollection<Commande> LesCommandes { get; set; }
         public ObservableCollection<Revendeur> LesRevendeurs { get; set; }
         public ObservableCollection<Employe> LesEmployes { get; set; }
+        public ObservableCollection<ModeTransport> LesTransports { get; set; }
         public WindowCommande(Commande uneCommande)
         {
             ChargeData();
@@ -35,6 +36,7 @@ namespace Pilot.Windows
             ChargeData();
             InitializeComponent();
             butValiderCommande.Content = act;
+            comboModeLivraison.ItemsSource = LesTransports;
         }
 
         private void ChargeData()
@@ -44,6 +46,7 @@ namespace Pilot.Windows
                 LesEmployes = new ObservableCollection<Employe>(new Employe().FindAll());
                 LesCommandes = new ObservableCollection<Commande>(new Commande().FindAll());
                 LesRevendeurs = new ObservableCollection<Revendeur>(new Revendeur().FindAll());
+                LesTransports = new ObservableCollection<ModeTransport>(new ModeTransport().FindAll());
                 this.DataContext = this;
             }
             catch (Exception ex)
@@ -56,7 +59,7 @@ namespace Pilot.Windows
 
         private void butValiderCommande_Click(object sender, RoutedEventArgs e)
         {
-            Employe employe = (Employe)dgEmploye.DataContext;
+            Employe employe = (Employe)dgEmploye.SelectedItem;
             Revendeur revendeur = (Revendeur)dgRevendeurs.SelectedItem;
             ModeTransport mt = (ModeTransport)comboModeLivraison.SelectedItem;
             Commande commande = new Commande(employe, mt, revendeur, DateTime.Parse(txtDateLivraison.Text));
